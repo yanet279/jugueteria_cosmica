0143dc6eee19785b2935dabc5167c5eec8ad7cc0
Yanet Soledad Choque Cayo <sol3127g@gmail.com>
2022-09-21 17:29:49 -0300

    Cree la carpeta template y el archivo single-page.js(SPA); en la carpeta guarde los archivos html excepto el index.

--- js/cart.js
+++ js/cart.js
@@ -1,99 +1,105 @@
-const cart = document.querySelector('.cart-button__row');
-const containerCart = document.querySelector('.cart-button__list tbody');
-const emptyCartBtn = document.querySelector('.cart-button__vaciar-carrito');
-const listArticles = document.querySelector('.cards-container');
-let articlesArray = [];
+window.addEventListener('load', ()=> {
+    const cart = document.querySelector('.cart-button__row');
+    const containerCart = document.querySelector('.cart-button__list tbody');
+    const emptyCartBtn = document.querySelector('.cart-button__vaciar-carrito');
+    const listArticles = document.querySelector('.cards-container');
+    let articlesArray = [];
 
-loadEventListeners();
+    console.log(cart);
+    console.log(articlesArray);
 
-function loadEventListeners(){
-    listArticles.addEventListener('click', addArticle);
-    cart.addEventListener('click', removeArticle);
-    emptyCartBtn.addEventListener('click', () => {
-        articlesArray= [];
-        cleanHTML();
-    })
-}
+    loadEventListeners();
 
-//eliminar un articulo del carrito
-function removeArticle(e){
-    if(e.target.classList.contains('cart-button__delete-article')){
-        const articuloId = e.target.getAttribute('data-id');
-        
-        //eliminar producto del carrito por data-id
-        articlesArray = articlesArray.filter( articulo => articulo.id !== articuloId);
+    function loadEventListeners() {
+        listArticles.addEventListener('click', addArticle);
+        cart.addEventListener('click', removeArticle);
+        emptyCartBtn.addEventListener('click', () => {
+            articlesArray= [];
+            cleanHTML();
+        })
+    }
+
+    //eliminar un articulo del carrito
+    function removeArticle(e){
+        if(e.target.classList.contains('cart-button__delete-article')){
+            const articuloId = e.target.getAttribute('data-id');
+            
+            //eliminar producto del carrito por data-id
+            articlesArray = articlesArray.filter( articulo => articulo.id !== articuloId);
+
+            cartHTML();
+        }
+    }
+
+    function addArticle(e){
+        e.preventDefault();
+        if(e.target.classList.contains('agregar-carrito')){
+            const selectedArticle = e.target.parentElement.parentElement.parentElement;
+            readArticleData(selectedArticle);
+        }
+    }
+
+    function readArticleData(articulo){
+        // objeto articulo actual
+        const infoArticle = {
+            imagen: articulo.querySelector('img').src,
+            titulo: articulo.querySelector('.card__heading').textContent,
+            precio: articulo.querySelector('.card__price-final').textContent,
+            id:     articulo.querySelector('a').getAttribute('data-id'),
+            cantidad:1
+        }
+        //console.table(infoArticulo);
+
+        const existe = articlesArray.some( articulo => articulo.id === infoArticle.id )
+
+        if(existe){
+            //actualizamos cantidad
+            const cantArticles = articlesArray.map(articulo => {
+                if(articulo.id === infoArticle.id){
+                    articulo.cantidad++;
+                    return articulo; //retorna objeto actualizado
+                }else{
+                    return articulo; //retorna no duplicados
+                }
+            })
+
+            articlesArray= [...cantArticles];
+        }else{
+
+            // agregar elementos al array=carrito
+            articlesArray = [...articlesArray, infoArticle];
+            console.log(articlesArray);
+        }
 
         cartHTML();
     }
-}
 
-function addArticle(e){
-    e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
-        const selectedArticle = e.target.parentElement.parentElement.parentElement;
-        readArticleData(selectedArticle);
-    }
-}
+    function cartHTML(){
 
-function readArticleData(articulo){
-    // objeto articulo actual
-    const infoArticle = {
-        imagen: articulo.querySelector('img').src ,
-        titulo: articulo.querySelector('.card__heading').textContent,
-        precio: articulo.querySelector('.card__price-final').textContent,
-        id:     articulo.querySelector('a').getAttribute('data-id'),
-        cantidad:1
-    }
-    //console.table(infoArticulo);
+        //limpiar html del carrito
+        cleanHTML();
 
-    const existe = articlesArray.some( articulo => articulo.id === infoArticle.id )
+        articlesArray.forEach( article => {
+            
+            const { imagen, titulo, precio, cantidad } = article;
+            const row = document.createElement('tr');
+            row.innerHTML = 
+            `
+                <td ><img src='${imagen}' width='100'</td>
+                <td>${titulo}</td>
+                <td>${precio}</td>
+                <td>${cantidad}</td>
+                <td><a href='#' class='cart-button__delete-article' data-id='${article.id}'>X</a></td>
+            `;
 
-    if(existe){
-        //actualizamos cantidad
-        const cantArticles = articlesArray.map( articulo => {
-            if(articulo.id === infoArticle.id){
-                articulo.cantidad++;
-                return articulo; //retorna objeto actualizado
-            }else{
-                return articulo; //retorna no duplicados
-            }
+            console.log(row);
+            //agregar el html
+            containerCart.appendChild(row);
         })
 
-        articlesArray= [...cantArticles];
-    }else{
-
-        // agregar elementos al array=carrito
-        articlesArray= [...articlesArray, infoArticle];
     }
 
-    cartHTML();
-}
-
-function cartHTML(){
-
-    //limpiar html del carrito
-    cleanHTML();
-
-    articlesArray.forEach( article => {
-        
-        const { imagen, titulo, precio, cantidad } = article;
-        const row = document.createElement('tr');
-        row.innerHTML = 
-        `
-            <td ><img src='${imagen}' width='100'</td>
-            <td>${titulo}</td>
-            <td>${precio}</td>
-            <td>${cantidad}</td>
-            <td><a href='#' class='cart-button__delete-article' data-id='${article.id}'>X</a></td>
-        `;
-
-        console.log(row);
-        //agregar el html
-        containerCart.appendChild(row);
-    })
-
-}
-
-function cleanHTML(){
-    containerCart.innerHTML = ''; 
-}
+    function cleanHTML(){
+        containerCart.innerHTML = ''; 
+    }
+});
--- js/main.js
+++ js/main.js
@@ -1,43 +1,45 @@
-let cartButtonContainer = document.querySelector('.main-header__cart-button-container');
-let inputCreated = false;
-let cartModalContainer = document.querySelectorAll('div')[0];
-let container = document.querySelector('.sections-carousel');
-let hamburgerContainer = document.querySelector('.main-header__hamburger-button-container');
-let navContainer = document.querySelector('.main.nav');
+window.addEventListener('load', ()=> {
+    let cartButtonContainer = document.querySelector('.main-header__cart-button-container');
+    // let inputCreated = false;
+    // let cartModalContainer = document.querySelectorAll('div')[0];
+    // let container = document.querySelector('.sections-carousel');
+    let hamburgerContainer = document.querySelector('.main-header__hamburger-button-container');
+    // let navContainer = document.querySelector('.main.nav');
 
-let flag;
+    let flag;
 
-// let closeNotVisible = () => cartModalContainer.innerHTML = ''
+    // let closeNotVisible = () => cartModalContainer.innerHTML = ''
 
-cartButtonContainer.addEventListener('click', e => {
-    const botonCarrito = e.target.parentElement;
-    console.log(e.target)
-    if(flag==0){
-        console.log(1)
-        flag=1;
-        botonCarrito.querySelector('.cart-button__row').style.display = "none";
-    }else{
-        botonCarrito.querySelector('.cart-button__row').style.display = "block";
-        console.log(2)
-        flag=0;
-    }
-    
-})
+    cartButtonContainer.addEventListener('click', e => {
+        const botonCarrito = e.target.parentElement;
+        if(flag==0){
+            console.log(1)
+            flag=1;
+            botonCarrito.querySelector('.cart-button__row').style.display = "none";
+        }else{
+            botonCarrito.querySelector('.cart-button__row').style.display = "block";
+            console.log(2)
+            flag=0;
+        }
+        
+    })
 
-document.addEventListener('keydown', e => {
-    console.log(e.key)
-    const boton = document.querySelector('.cart-button__row');
-    if(e.key === 'Escape'){
-        boton.style.display="none";
-        flag=0;
-    }
-})
+    document.addEventListener('keydown', e => {
+        console.log(e.key)
+        const boton = document.querySelector('.cart-button__row');
+        if(e.key === 'Escape'){
+            boton.style.display="none";
+            flag = 1;
+        }
+    })
 
-hamburgerContainer.addEventListener('click', e => {
-    const topBread = document.querySelector('.hamburger-button__top-bread') 
-    topBread.classList.toggle('hamburger-button__top-bread--close')
-    const meat = document.querySelector('.hamburger-button__meat') 
-    meat.classList.toggle('hamburger-button__meat--close')
-    const bottonBread = document.querySelector('.hamburger-button__botton-bread') 
-    bottonBread.classList.toggle('hamburger-button__botton-bread--close')
+    hamburgerContainer.addEventListener('click', e => {
+        const topBread = document.querySelector('.hamburger-button__top-bread') 
+        topBread.classList.toggle('hamburger-button__top-bread--close')
+        const meat = document.querySelector('.hamburger-button__meat') 
+        meat.classList.toggle('hamburger-button__meat--close')
+        const bottonBread = document.querySelector('.hamburger-button__botton-bread') 
+        bottonBread.classList.toggle('hamburger-button__botton-bread--close')
+    })
+
 })
